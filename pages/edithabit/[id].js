8662f75b26bc7628db8641b807080c08c888b723
Felix Jungclaus <felix.jungcl@gmail.com>
2023-05-15 10:25:01 +0200

    add_function_EditHabitPage

---
+++ pages/edithabit/[id].js
@@ -0,0 +1,25 @@
+import EditHabitForm from "@/components /EditHabitForm";
+import useStore from "@/Data/store";
+import { useRouter } from "next/router";
+
+export default function EditHabitPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const habit = useStore((State) => State.habits.find((h) => h.id === id));
+  const updateHabit = useStore((state) => state.updateHabit);
+
+  const handleSubmit = (updateHabit) => {
+    updateHabit(updateHabit);
+  };
+
+  if (!habit) {
+    return <p>Habit not found </p>;
+  }
+
+  return (
+    <div>
+      <h1>Edit habit</h1>
+      <EditHabitForm habit={habit} onSubmit={handleSubmit} />
+    </div>
+  );
+}
