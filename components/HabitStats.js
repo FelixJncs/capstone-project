4845e8fbc0abcbd75061af0261b01f700cd7f121
Felix Jungclaus <felix.jungcl@gmail.com>
2023-05-26 16:48:17 +0200

    deleted_redundant_code

--- components/HabitStats.js
+++ components/HabitStats.js
@@ -1,5 +1,7 @@
 import useHabitsStore from "@/store";
 import styled from "styled-components";
+import { StyledLink } from "./StyledLink";
+import { StyledTitle } from "./StyledTitle";
 
 const StyledHabitStatsList = styled.ul`
   list-style-type: none;
@@ -20,19 +22,24 @@ const HabitStats = () => {
   const habits = useHabitsStore((state) => state.habits);
 
   return (
-    <StyledHabitStatsList>
-      {habits.map((habit) => {
-        return (
-          <StyledHabitStatsCard key={habit.id}>
-            <p>{habit.name}</p>
-            <p>Current Streak: {habit.streakCount}</p>{" "}
-            {/* Display current streak */}
-            <p>Longest Streak: {habit.longestStreak}</p>{" "}
-            {/* Display longest streak */}
-          </StyledHabitStatsCard>
-        );
-      })}
-    </StyledHabitStatsList>
+    <>
+      <StyledTitle>Unhabit</StyledTitle>
+      <h2>Habit Stats</h2>
+      <StyledHabitStatsList>
+        {habits.map((habit) => {
+          return (
+            <StyledHabitStatsCard key={habit.id}>
+              <p>{habit.name}</p>
+              <p>Current Streak: {habit.streakCount}</p>{" "}
+              {/* Display current streak */}
+              <p>Longest Streak: {habit.longestStreak}</p>{" "}
+              {/* Display longest streak */}
+            </StyledHabitStatsCard>
+          );
+        })}
+      </StyledHabitStatsList>
+      <StyledLink href="/">back</StyledLink>
+    </>
   );
 };
 
--- pages/habitstats/index.js
+++ pages/habitstats/index.js
@@ -1,42 +1,10 @@
-import useHabitsStore from "@/store";
-import { StyledLink } from "@/components/StyledLink";
-import styled from "styled-components";
-import { StyledTitle } from "@/components/StyledTitle";
-import Link from "next/link";
-
-const StyledHabitStatsList = styled.ul`
-  list-style-type: none;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  justify-content: center;
-  padding-left: 0;
-`;
-
-const StyledHabitStatsCard = styled.li`
-  background-color: #f5f5f5;
-  padding: 1rem;
-  border-radius: 4px;
-`;
+import HabitStats from "@/components/HabitStats";
 
 const HabitStatsPage = () => {
-  const habits = useHabitsStore((state) => state.habits);
-
   return (
-    <>
-      <StyledHabitStatsList>
-        <StyledTitle>Unhabit</StyledTitle>
-        <h2>Habit Stats</h2>
-        {habits.map((habit) => (
-          <StyledHabitStatsCard key={habit.id}>
-            <p>{habit.name}</p>
-            <p>Current Streak: {habit.streakCount}</p>
-            <p>Longest Streak: {habit.longestStreak}</p>
-          </StyledHabitStatsCard>
-        ))}
-      </StyledHabitStatsList>
-      <StyledLink href="/">Back</StyledLink>
-    </>
+    <div>
+      <HabitStats />
+    </div>
   );
 };
 
