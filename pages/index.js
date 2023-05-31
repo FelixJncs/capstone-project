40cea699eecd83a4237f5b9ab369f57ae96a1738
Felix Jungclaus <felix.jungcl@gmail.com>
2023-05-31 15:00:10 +0200

    deleted_redundant_links_PagesIndex

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import HabitList from "@/components/HabitList";
 import styled from "styled-components";
-import Link from "next/link";
 import { StyledTitle } from "@/components/StyledTitle";
 
 const StyledHabitListContainer = styled.div`
@@ -11,28 +10,12 @@ const StyledHabitListContainer = styled.div`
   padding: 2rem;
 `;
 
-const StyledHomepageLink = styled(Link)`
-  display: inline-block;
-  padding: 1rem 2rem;
-  background-color: beige;
-  border-radius: 1rem;
-  box-shadow: 0 0.2rem 0.2rem rgba(0, 0, 0, 0.1);
-  border: none;
-  text-decoration: none;
-  color: black;
-  font-weight: bold;
-  margin: 0 2rem 2rem;
-  cursor: pointer;
-`;
 export default function Home() {
   return (
     <>
       <StyledTitle>Unhabit</StyledTitle>
       <StyledHabitListContainer>
         <HabitList />
-
-        <StyledHomepageLink href="/addhabit">Add New Habit</StyledHomepageLink>
-        <StyledHomepageLink href="/habitstats">Stats </StyledHomepageLink>
       </StyledHabitListContainer>
     </>
   );
